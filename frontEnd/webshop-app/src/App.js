cfcce349efdde594d9ae637c79c3499dde3741ac
P.Patrik <fake email>
2020-04-16 20:27:41 +0200

    product detail

--- frontEnd/webshop-app/src/App.js
+++ frontEnd/webshop-app/src/App.js
@@ -1,25 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Home from '../src/components/home-component'
+import Products from '../src/components/products-component'
+import Orders from '../src/components/orders-component'
+import ProductDetail from '../src/components/product-detail'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <div className="row">
+      <h3 className="col-3">WebShop</h3>
+      <div className="col-3">
+      <nav>
+        <ul>
+          <li className="px-5">
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link className="px-5" to="/products">Products</Link>
+          </li>
+          <li>
+            <Link className="px-5" to="/orders">Orders</Link>
+          </li>
+        </ul>
+      </nav>
+      </div>
+      </div>
+      <div className="row">
+        <div className="col-9">
+      <Switch>
+      <Route path="/products/:id" component={ProductDetail} >
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/orders">
+          <Orders />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+        </div>
+      </div>
+  </Router>
   );
 }
 
---
+++ frontEnd/webshop-app/src/components/product-detail.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+
+export default class ProductDetail extends Component{
+    constructor(){
+        super()
+        this.state = {
+            product: undefined
+        }
+
+    }
+
+    componentDidMount() {
+        fetch(`http://44228aa1.ngrok.io/products/${this.props.match.params.id}`)
+        .then(res => res.json())
+        .then(product => {
+          console.log(product)
+          this.setState({product})
+        })
+    }
+    
+    render(){
+        return(
+           <div>
+{this.state.product ? 
+                (
+                    <div>
+               <div>
+                <p>placeholder for image gallery</p>
+               <p>{this.state.product.name}</p>
+               <p>{this.state.product.price}</p>
+               <button type="button">Add to cart</button>
+               <p>{this.state.product.shortSpecs}</p>
+               </div>
+               <p>{this.state.product.specs}</p>
+               <p>{this.state.product.recommendations}</p>
+               </div>)
+               : "Loading"}
+
+           </div>
+        )
+    }
+}
